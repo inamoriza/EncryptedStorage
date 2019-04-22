2242751681d5388f1f1502ad0ad1adc22a966043
IkerNebotAmoriza <ikernebot@gmail.com>
2019-04-22 17:45:10 +0200

    Importado proyecto autogenerado

---
+++ src/main/java/com/crypt/storage/CryptStorageApplication.java
@@ -0,0 +1,13 @@
+package com.crypt.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptStorageApplication.class, args);
+	}
+
+}
