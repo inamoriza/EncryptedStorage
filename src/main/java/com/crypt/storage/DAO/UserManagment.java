9f5264f134df4854700fd7898a0c14ae88e878f1
IkerNebotAmoriza <ikernebot@gmail.com>
2019-05-05 17:36:12 +0200

    Cambiada localización de la base de datos local
    Cambiada la localizacion de la base de datos a una fuera de la carpeta raiz del servidor.
    
    He cambiado el signUp.html para que el metodo del formulario sea POST.
    
    Cambiado el controller de signUp para que formatee el fichero  "userList" adecuadamente.

--- src/main/java/com/crypt/storage/DAO/UserManagment.java
+++ src/main/java/com/crypt/storage/DAO/UserManagment.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class UserManagment {
 
-    private final String listURL = "com/crypt/storage/info/userList.txt";
+    private final String listURL = "C:/esdb/userList.txt";
     private BufferedWriter writer;
     private BufferedReader read;
 
@@ -13,26 +13,24 @@ public class UserManagment {
     }
 
     public void saveUser(String username, String hash) throws IOException {
-        String user= username+":"+hash;
-        writer = new BufferedWriter(new FileWriter(listURL));
+        String user = username + ":" + hash;
+        writer = new BufferedWriter(new FileWriter(listURL, true));
         writer.write(user);
+        writer.newLine();
+        writer.close();
     }
+
     public boolean checkUser(String username, String hash) throws IOException {
-        String user = username+":"+hash;
+        String user = username + ":" + hash;
         read = new BufferedReader(new FileReader(listURL));
 
         String next = read.readLine();
-        while(next!= null){
-            if(next.equals(user))
+        while (next != null) {
+            if (next.equals(user))
                 return true;
             next = read.readLine();
         }
+        read.close();
         return false;
     }
-
-
-
-
-
-
 }
