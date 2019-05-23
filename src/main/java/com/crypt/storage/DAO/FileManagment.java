8b249eb714e49ff9234b7415bfcbc1ebf07cef8c
XxCatxX <al364403@uji.es>
2019-05-23 18:54:52 +0200

    Create FileManagment.java

---
+++ src/main/java/com/crypt/storage/DAO/FileManagment.java
@@ -0,0 +1,51 @@
+package com.crypt.storage.DAO;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+public class FileManagment {
+
+    private static final String ALGORITHM = "AES";
+    private static final byte[] salt = new byte[8];
+
+    public String encrypt(String password, File file) throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        SecureRandom srandom = new SecureRandom();
+        srandom.nextBytes(salt);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256);
+        SecretKey tmp = factory.generateSecret(spec);
+        SecretKeySpec skey = new SecretKeySpec(tmp.getEncoded(), ALGORITHM);
+
+        byte[] iv = new byte[128/8];
+        srandom.nextBytes(iv);
+        IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+        Cipher ci;
+
+        try {
+            ci = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            ci.init(Cipher.ENCRYPT_MODE, skey, ivspec);
+        } catch (NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
+            System.out.println("Bad encrypt");
+            e.printStackTrace();
+        }
+        
+        //final byte[] resultBytes = ci.doFinal();
+
+        //return Base64.getMimeEncoder().encodeToString(resultBytes);
+
+        return "";
+    }
+
+}
