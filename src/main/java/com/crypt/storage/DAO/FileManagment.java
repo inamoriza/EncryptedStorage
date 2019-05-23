59a62d0ead4d8e8c17e93434bec81e3cad2fa75a
Iker Nebot Amoriza <ikernebot@gmail.com>
2019-05-24 00:14:24 +0200

    Añadidos metodos Encrypt, Decrypt y GetSecret

--- src/main/java/com/crypt/storage/DAO/FileManagment.java
+++ src/main/java/com/crypt/storage/DAO/FileManagment.java
@@ -5,50 +5,109 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
+import java.nio.file.Files;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
 import java.security.spec.KeySpec;
-import java.util.Base64;
+import java.util.ArrayList;
 
 public class FileManagment {
 
+    private static final String SK_FACTORY_INSTANCE = "PBKDF2WithHmacSHA256";
+    private static final String CIPHER_INSTANCE = "AES/CBC/PKCS5Padding";
     private static final String ALGORITHM = "AES";
-    private static final byte[] salt = new byte[8];
+    private static final int ITERATIONS = 65536;
+    private static final int SIZE = 256;
 
-    public String encrypt(String password, File file) throws NoSuchAlgorithmException, InvalidKeySpecException {
-
-        SecureRandom srandom = new SecureRandom();
-        srandom.nextBytes(salt);
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256);
-        SecretKey tmp = factory.generateSecret(spec);
-        SecretKeySpec skey = new SecretKeySpec(tmp.getEncoded(), ALGORITHM);
-
-        byte[] iv = new byte[128/8];
-        srandom.nextBytes(iv);
-        IvParameterSpec ivspec = new IvParameterSpec(iv);
-
-        Cipher ci=null;
-
+    public SecretKeySpec getSecretKey(String password) {
+        byte[] salt = new byte[8];
+        SecureRandom sRandom = new SecureRandom();
+        sRandom.nextBytes(salt);
         try {
-            ci = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            ci.init(Cipher.ENCRYPT_MODE, skey, ivspec);
-        } catch (NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
-            System.out.println("Bad encrypt");
-            e.printStackTrace();
+            SecretKeyFactory factory = SecretKeyFactory.getInstance(SK_FACTORY_INSTANCE);
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, ITERATIONS, SIZE);
+            SecretKey k = factory.generateSecret(spec);
+            return new SecretKeySpec(k.getEncoded(), ALGORITHM);
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println("Factory specified algorithm is not valid.");
+            ex.printStackTrace();
+        } catch (InvalidKeySpecException ex) {
+            System.out.println("Secret Key specification is not valid.");
+            ex.printStackTrace();
         }
-
-        byte[] bytes = new byte[(int) file.length()];
-
-        try {
-            final byte[] resultBytes = ci.doFinal(bytes);
-        } catch (IllegalBlockSizeException | BadPaddingException e) {
-            e.printStackTrace();
-        }
-
-        //return Base64.getMimeEncoder().encodeToString(resultBytes);
-
-        return "";
+        return null;
     }
 
+    public ArrayList<byte[]> encryptFile(SecretKeySpec secret, File file) {
+        ArrayList<byte[]> res = new ArrayList<>();
+        try {
+            Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
+            cipher.init(Cipher.ENCRYPT_MODE, secret);
+            AlgorithmParameters params = cipher.getParameters();
+            byte[] initVector = params.getParameterSpec(IvParameterSpec.class).getIV();
+            byte[] cipherText = cipher.doFinal(Files.readAllBytes(file.toPath()));
+            res.add(initVector);
+            res.add(cipherText);
+            return res;
+
+        } catch (NoSuchPaddingException ex) {
+            System.out.println("Specified cipher instance padding is not valid.");
+            ex.printStackTrace();
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println("Specified algorithm is not valid.");
+            ex.printStackTrace();
+        } catch (InvalidKeyException ex) {
+            System.out.println("Specified key is not valid.");
+            ex.printStackTrace();
+        } catch (InvalidParameterSpecException ex) {
+            System.out.println("Specified parameter spec is not valid.");
+            ex.printStackTrace();
+        } catch (IllegalBlockSizeException ex) {
+            System.out.println("Specified block size is not valid.");
+            ex.printStackTrace();
+        } catch (BadPaddingException ex) {
+            System.out.println("Specified padding is not valid.");
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            System.out.println("Error reading file.");
+            ex.printStackTrace();
+        }
+        return res;
+    }
+
+    public byte[] decryptFile(SecretKeySpec secret, byte[] initVector, byte[] cipherText) {
+        try {
+            Cipher cipher = Cipher.getInstance(CIPHER_INSTANCE);
+            cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(initVector));
+
+            CipherInputStream cipherInput = new CipherInputStream(new ByteArrayInputStream(initVector), cipher);
+            ByteArrayOutputStream cipherOutput = new ByteArrayOutputStream();
+
+            int len;
+            byte[] buffer = new byte[4096];
+            while ((len = cipherInput.read(buffer, 0, buffer.length)) != -1) {
+                cipherOutput.write(buffer, 0, len);
+            }
+            cipherOutput.flush();
+            return cipherOutput.toByteArray();
+
+        } catch (IOException ex) {
+            System.out.println("IO error.");
+            ex.printStackTrace();
+        } catch (NoSuchPaddingException ex) {
+            System.out.println("Specified cipher instance padding is not valid.");
+            ex.printStackTrace();
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println("Specified algorithm is not valid.");
+            ex.printStackTrace();
+        } catch (InvalidKeyException ex) {
+            System.out.println("Specified key is not valid.");
+            ex.printStackTrace();
+        } catch (InvalidAlgorithmParameterException ex) {
+            System.out.println("Specified algorithm parameter is not valid.");
+            ex.printStackTrace();
+        }
+        return null;
+    }
 }
